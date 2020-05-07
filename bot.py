9af1ce7b0f9bd1154c04c4dc9303d56cc4ae934e
Maruf <36636180+Madraasasa@users.noreply.github.com>
2020-05-07 11:56:53 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 import telebot
 from pytube import YouTube as yt
+import telegraph
 bot = telebot.TeleBot('986722664:AAEx7v_gw8gZvKS7VEZojP2P5JdKfBmoOEQ')
 bot.send_message(120929625, 'test')
 a = yt('https://www.youtube.com/watch?v=5X5ZLWdAnt4').streams[0].download()
 # print(a)
-f = open('19 Уроки React JS (route browser-router маршрутизация) - react курсы бесплатно.mp4', 'rb')
-bot.send_video(120929625, f.read(), supports_streaming=True)
+# f = open('19 Уроки React JS (route browser-router маршрутизация) - react курсы бесплатно.mp4', 'rb')
+m = telegraph.upload.upload_file('19 Уроки React JS (route browser-router маршрутизация) - react курсы бесплатно.mp4')
+bot.send_video(120929625, f'https://telegra.ph{m[0]}', supports_streaming=True)
 bot.send_message(120929625, 'всё')
