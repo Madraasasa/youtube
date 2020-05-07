b978f64f2deb91658763f9c04a7728a55a3989f5
Maruf <36636180+Madraasasa@users.noreply.github.com>
2020-05-07 12:14:10 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ from pytube import YouTube as yt
 import telegraph
 bot = telebot.TeleBot('986722664:AAEx7v_gw8gZvKS7VEZojP2P5JdKfBmoOEQ')
 bot.send_message(120929625, 'test')
-a = yt('https://www.youtube.com/watch?v=5X5ZLWdAnt4').streams[0].download()
+a = yt('https://www.youtube.com/watch?v=5X5ZLWdAnt4').streams[1].download()
 # print(a)
 f = open('19 Уроки React JS (route browser-router маршрутизация) - react курсы бесплатно.mp4', 'rb')
-m = telegraph.upload.upload_file(f.read())
-bot.send_video(120929625, f'https://telegra.ph{m[0]}', supports_streaming=True)
+# m = telegraph.upload.upload_file(f.read())
+bot.send_video(120929625, f.read(), supports_streaming=True)
 bot.send_message(120929625, 'всё')
